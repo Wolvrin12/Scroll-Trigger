96fbf51ed007132a11c157cb61b030f2374fb794
Wolvrin12 <anouarchlih31@gmail.com>
2023-07-20 17:48:09 +0100

    CleanUp

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-gsap.timeline({
+gsap
+  .timeline({
     scrollTrigger: {
-        trigger: ".box",
-        start: "center center",
-        end: "bottom top",
-        markers: false,
-        scrub: true,
-        pin: true
-    }
-})
-.from(".text1", { x : innerWidth * 1})
-.from(".text2", { x : innerWidth * -1})
-.from(".text3", { x : innerWidth * 1})
-.from(".logo", { 
-    y : innerHeight * 1,
-    rotate : 360
-})
+      trigger: ".box",
+      start: "center center",
+      end: "bottom top",
+      markers: false,
+      scrub: true,
+      pin: true,
+    },
+  })
+  .from(".text1", { x: innerWidth * 1 })
+  .from(".text2", { x: innerWidth * -1 })
+  .from(".text3", { x: innerWidth * 1 })
+  .from(".logo", {
+    y: innerHeight * 1,
+    rotate: 360,
+  });
 
-gsap.timeline({
+gsap
+  .timeline({
     scrollTrigger: {
-        trigger: ".box2",
-        start: "center center",
-        end: "bottom top",
-        markers: false,
-        scrub: true,
-        pin: true
-    }
-})
-.from(".box2", { opacity: 0 })
-.from(".text4", { y : innerHeight * 1})
-.from(".text5", { y : innerHeight * 1})
-.from(".text6", { y : innerHeight * 1})
+      trigger: ".box2",
+      start: "center center",
+      end: "bottom top",
+      markers: false,
+      scrub: true,
+      pin: true,
+    },
+  })
+  .from(".box2", { opacity: 0 })
+  .from(".text4", { y: innerHeight * 1 })
+  .from(".text5", { y: innerHeight * 1 })
+  .from(".text6", { y: innerHeight * 1 });
